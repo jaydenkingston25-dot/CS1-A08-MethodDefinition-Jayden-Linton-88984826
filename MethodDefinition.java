46f3fbe9d64c1394606510af7937109ce40654ec
jaydenkingston25-dot <jaydenkingston25@gmail.com>
2026-06-24 15:35:13 -0600

    MethodDefinition.java

--- MethodDefinition.java
+++ MethodDefinition.java
@@ -8,75 +8,75 @@ public class MethodDefinition {
     // Leave them commented out (//) so your IDE doesn't complain about missing return statements!
     // Do NOT write the logic inside the curly braces.
 
-    // 1. A method named calculateSum that takes three int values as parameters, i.e., x, y, and z. 
+    // 1. A method named calculateSum that takes three int values as parameters, i.e., x, y, and z.
     // The method returns the total sum of these three values.
     // public static int calculateSum(int x, int y, int z) {}
-
-    // 2. A method named getInitials that takes two Strings as parameters, i.e., s1 and s2. 
+ 
+    // 2. A method named getInitials that takes two Strings as parameters, i.e., s1 and s2.
     // The method returns a String containing the first letters of the two parameters.
-    // TODO: Write signature here
-
+    // public static String getInitials(String s1, String s2) {}
+ 
     // 3. A method named printAll that will take a String s as a parameter and will print all the characters from the String.
-    // TODO: Write signature here
-
-    // 4. A method named isPositive that takes an int n as a parameter. 
+    // public static void printAll(String s) {}
+ 
+    // 4. A method named isPositive that takes an int n as a parameter.
     // The method returns true if the number is positive, false otherwise.
-    // TODO: Write signature here
-
+    // public static boolean isPositive(int n) {}
+ 
     // 5. A method named printReverse that will print in reverse order a String provided as a parameter.
-    // TODO: Write signature here
-
-    // 6. A method named getAge that will take an int as a parameter named year. 
+    // public static void printReverse(String s) {}
+ 
+    // 6. A method named getAge that will take an int as a parameter named year.
     // The method shall return the current age based on the parameter year.
-    // TODO: Write signature here
-
-    // 7. A method named printEvenNumbers that will take an int n as a parameter. 
+    // public static int getAge(int year) {}
+ 
+    // 7. A method named printEvenNumbers that will take an int n as a parameter.
     // The method shall print all the even numbers between 0 and the number n.
-    // TODO: Write signature here
-
-    // 8. A method named printOddNumbers that will take an int n as a parameter. 
+    // public static void printEvenNumbers(int n) {}
+ 
+    // 8. A method named printOddNumbers that will take an int n as a parameter.
     // The method shall print all the odd numbers between 0 and the number n.
-    // TODO: Write signature here
-
-    // 9. A method named printEvensByRange that will take two int numbers as parameters (i.e., x and y). 
+    // public static void printOddNumbers(int n) {}
+ 
+    // 9. A method named printEvensByRange that will take two int numbers as parameters (i.e., x and y).
     // The method shall print all the even numbers that appear between x and y numbers.
-    // TODO: Write signature here
-
-    // 10. A method named printFibonacciNumbers that will take a parameter n. 
+    // public static void printEvensByRange(int x, int y) {}
+ 
+    // 10. A method named printFibonacciNumbers that will take a parameter n.
     // The method shall print the Fibonacci sequence from 1 all the way to the number n.
-    // TODO: Write signature here
-
+    // public static void printFibonacciNumbers(int n) {}
+ 
     // 11. A method named getAVG that will take three ints as parameters and return the average of the three of them.
-    // TODO: Write signature here
-
-    // 12. A method named readNumbers, the method will take a File object as a parameter, 
+    // public static double getAVG(int x, int y, int z) {}
+ 
+    // 12. A method named readNumbers, the method will take a File object as a parameter,
     // representing the file containing numbers and will print each number from the file.
-    // TODO: Write signature here
-
+    // public static void readNumbers(File file) {}
+ 
     // 13. A method named getThird, the method will take a String as a parameter and will return the third character from the given String.
-    // TODO: Write signature here
-
+    // public static char getThird(String s) {}
+ 
     // 14. A private method named setX, that will take a double as a parameter that will initialize the value to another int.
-    // TODO: Write signature here
-
+    // private static void setX(double x) {}
+ 
     // 15. A private method named getTime that will return the current time object Date.
-    // TODO: Write signature here
-
-    // 16. A protected method named isUpperCase that will take a character as a parameter. 
+    // private static Date getTime() {}
+ 
+    // 16. A protected method named isUpperCase that will take a character as a parameter.
     // The method determines if the character is an uppercase or not.
-    // TODO: Write signature here
-
+    // protected static boolean isUpperCase(char c) {}
+ 
     // 17. A protected method named currentTimeMillis that return the current time in milliseconds.
-    // TODO: Write signature here
-
+    // protected static long currentTimeMillis() {}
+ 
     // 18. A protected method named nanoTime that return the current time in nanoseconds.
-    // TODO: Write signature here
-
+    // protected static long nanoTime() {}
+ 
     // 19. A method called random that returns a value with a positive sign, greater than or equal to 0.0, and less than 1.0.
-    // TODO: Write signature here
-
-    // 20. A method named processArgs that will take an array of strings called args from the main method. 
+    // public static double random() {}
+ 
+    // 20. A method named processArgs that will take an array of strings called args from the main method.
     // The method shall return the length of the args (i.e., the numbers of elements in args).
-    // TODO: Write signature here
+    // public static int processArgs(String[] args) {}
 
 }
